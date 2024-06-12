ba1c25f2bb7f8cffb7212edd4da46d16d2d9f5a3
NgocVuMinh <ngc.vux@gmail.com>
2024-06-12 11:33:00 +0700

    fix path to bam files

--- sam2bam.sh
+++ sam2bam.sh
@@ -31,13 +31,13 @@ while IFS= read -r sample; do
     
     # Sorting BAM
     echo "$(date) - Sorting BAM file: ${sample}" 2>&1 | tee -a $log_file_path
-    samtools sort -@ 3 "./mapped/${sample}.bam" -o "./mapped/${sample}_sorted.bam" 2>&1 | tee -a $log_file_path
+    samtools sort -@ 3 "./processed/${sample}.bam" -o "./processed/${sample}_sorted.bam" 2>&1 | tee -a $log_file_path
     echo "$(date) - Sorting completed for ${sample}" 2>&1 | tee -a $log_file_path
     printf '\n' 2>&1 | tee -a "$log_file_path"
 
     # Indexing BAM
     echo "$(date) - Indexing BAM file: ${sample}" 2>&1 | tee -a $log_file_path
-    samtools index -@ 3 "./mapped/${sample}_sorted.bam" 2>&1 | tee -a $log_file_path 2>&1 | tee -a $log_file_path
+    samtools index -@ 3 "./processed/${sample}_sorted.bam" 2>&1 | tee -a $log_file_path 2>&1 | tee -a $log_file_path
     echo "$(date) - Indexing completed for ${sample}" 2>&1 | tee -a $log_file_path
     printf '\n' 2>&1 | tee -a "$log_file_path"
 
